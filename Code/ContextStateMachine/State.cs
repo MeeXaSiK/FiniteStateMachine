a7e73a3116ad8d0d8e19ebbaff21dfcd81fe4e6e
Night Train Code <87555873+MeeXaSiK@users.noreply.github.com>
2023-05-25 14:01:58 +0700

    Update State.cs

--- Code/ContextStateMachine/State.cs
+++ Code/ContextStateMachine/State.cs
@@ -5,6 +5,11 @@
         public TInitializer Initializer { get; }
         public bool IsActive { get; set; }
 
+        protected State(TInitializer stateInitializer)
+        {
+            Initializer = stateInitializer;
+        }
+
         public virtual void OnEnter() { }
         public virtual void OnRun() { }
         public virtual void OnExit() { }
